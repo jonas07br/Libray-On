fc96e347f86b1fea46806e77b41f802600a9ea90
jonas07br <jonasrafa07@gmail.com>
2022-11-03 23:21:15 -0300

    login

--- processalogin.php
+++ processalogin.php
@@ -1,7 +1,22 @@
 <?php 
 session_start();
-if(isset($_POST['enviar']){
+if(isset($_POST['enviar'])){
+
+    $email=isset($_POST['email'])? $_POST['email']:"";
+    $senha=isset($_POST['senha'])? $_POST['senha']:"";
+        if ($email=="adm@123" && $senha=="adm"){
+            header("Location:telainicio.html");
+        }
+        else {
+            header("Location:telalogin.html");
+        }
+        
     
+
+}
+else{
+    
+    header("Location:telalogin.html");
 }
 
 
