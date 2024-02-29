499065e17770774525c5700a22a3942615df9cff
jonas07br <115741544+jonas07br@users.noreply.github.com>
2024-02-29 12:22:17 -0300

    Update processaaddlivro.php
    Organização do código

--- processapags/processaaddlivro.php
+++ processapags/processaaddlivro.php
@@ -3,33 +3,31 @@ session_start();
 if(isset($_POST['addlivro'])){
 	//conexao
 	include ("../conexao.php");
+	$nomelivro=isset($_POST["nomelivro"])?$_POST["nomelivro"]:"";
+    	$autor=isset($_POST["autor"])?$_POST["autor"]:"";
+    	$genero=isset($_POST["genero"])?$_POST["genero"]:"";
+    	$cod=isset($_POST["codlivro"])?$_POST["codlivro"]:"";
+	$desc=isset($_POST["descricao"])?$_POST["descricao"]:"";
+	//verificar se o livro existe
+	$sql="SELECT * FROM livros WHERE nomelivro = '$nomelivro'; ";
+    	$resultado = mysqli_query($con,$sql);
+    	$row=mysqli_num_rows($resultado);
+    	if ($row==1){
+        	header("location: ../telainicio.php?s=addl&p=adderro");
+    	}
 	
-	//$email = isset($_POST["email"])?$_POST["email"]:"";
-	//$nome = isset($_POST["nome"])?$_POST["nome"]:"";
-    //$password = isset($_POST["senha"])?$_POST["senha"]:"";
-	//verificar se o email esta sendo usado
-	//$sql="SELECT * FROM usuario WHERE email = '$email'; ";
-    //$resultado = mysqli_query($con,$sql);
-    //$row=mysqli_num_rows($resultado);
-    //if ($row==1){
-        //header("location: telacadastro.php?s=e");
-    //}
-	//preparar um SQL para executar no banco
-    $nomelivro=isset($_POST["nomelivro"])?$_POST["nomelivro"]:"";
-    $autor=isset($_POST["autor"])?$_POST["autor"]:"";
-    $genero=isset($_POST["genero"])?$_POST["genero"]:"";
-    $cod=isset($_POST["codlivro"])?$_POST["codlivro"]:"";
-	if ($stmt = mysqli_prepare($con, "insert into livros(nomelivro,autor,genero,codlivro) values(?,?,?,?)")) {
+    
+	if ($stmt = mysqli_prepare($con, "insert into livros(nomelivro,autor,genero,codlivro,descricao) values(?,?,?,?,?)")) {
 		//vincular valores as interrogacoes (?)
-		mysqli_stmt_bind_param($stmt,'sssi',$nomelivro,$autor,$genero,$cod);
-	    //efetiva e executa a SQL no banco, i.e., insere
-	    $status = mysqli_stmt_execute($stmt);
+		mysqli_stmt_bind_param($stmt,'sssis',$nomelivro,$autor,$genero,$cod,$desc);
+	   	 //efetiva e executa a SQL no banco, i.e., insere
+	    	$status = mysqli_stmt_execute($stmt);
 		if ($status === false) {
 			trigger_error($stmt->error, E_USER_ERROR);
-		  }
-	    //fecha o statement
-	    mysqli_stmt_close($stmt);
+		}
+	   	 //fecha o statement
+	   	 mysqli_stmt_close($stmt);
 	}
 	mysqli_close($con);
-    header("location: ../telainicio.php?s=addl");
+    	header("location: ../telainicio.php?s=addl&p=addok");
 }
