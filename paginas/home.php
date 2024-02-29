55131db073fbc54bac56d6d1f5b01e8624978d83
jonas07br <115741544+jonas07br@users.noreply.github.com>
2024-02-29 12:27:30 -0300

    Update home.php

--- paginas/home.php
+++ paginas/home.php
@@ -7,7 +7,7 @@ if ( $_SESSION["controle"]!="ok"){
         <div class="row">
           <div class="col-sm-3 " >
             <div class="card" style="width: 18rem;">
-              <a href="telainicio.php?s=addl"><img src="imgs/livro.png"   class="card-img-top "></a>
+              <a href="telainicio.php?s=addl"><img src="imgs/add.png"   class="card-img-top "></a>
               <div class="card-body">
                 <h4 class="card-text text-center">Adicionar Livro</h4>
               </div>
@@ -15,15 +15,15 @@ if ( $_SESSION["controle"]!="ok"){
         </div>
         <div class="col-sm-3">
           <div class="card" style="width: 18rem;">
-            <a href="processapags/processalivros.php" name="mostrar"><img src="imgs/livro.png"   class="card-img-top "></a>
+            <a href="processapags/processalivros.php" name="mostrar"><img src="imgs/liv.png"   class="card-img-top "></a>
             <div class="card-body">
-              <h4 class="card-text text-center">Livros</h4>
+              <h4 class="card-text text-center">Acervo</h4>
             </div>
           </div>
       </div>
         <div class="col-sm-3">
           <div class="card" style="width: 18rem;">
-            <a href="telainicio.php?s=emp"><img src="imgs/emprestimo.png"    class="card-img-top "></a> 
+            <a href="telainicio.php?s=emp"><img src="imgs/bo.png"    class="card-img-top "></a> 
             <div class="card-body">
               <h4 class="card-text text-center">Empréstimos</h4>
             </div>
@@ -31,9 +31,11 @@ if ( $_SESSION["controle"]!="ok"){
         </div>
         <div class="col-sm-3 ">
           <div class="card " style="width: 18rem; ">
-            <a href="telainicio.php?s=addu"> <img src="imgs/usuario.png"    class="card-img-top "></a> 
+            <a href="telainicio.php?s=addu"> <img src="imgs/user.png"    class="card-img-top "></a> 
             <div class="card-body">
-              <h4 class="card-text text-center">Adicionar usuário</h4>
+              <h4 class="card-text text-center">Gerenciar Usuários</h4>
             </div>
+          </div>
       </div>
-      </div>
+      
+</div>
